43250982913bd9d27dd5f2532bde2bcc481663df
Danillo Leão Deucher <danillo_10_@hotmail.com>
2024-12-05 10:00:29 -0300

    Microserviço de vendas

---
+++ src/test/java/br/com/c9tecnologia/msvenda/MsVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.c9tecnologia.msvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
